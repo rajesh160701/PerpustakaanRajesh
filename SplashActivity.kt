078bccb173464bdc586064b94732db3791cd37c0
rajesh160701 <rajesrahmanputra160701@gmail.com>
2026-01-31 19:49:26 +0700

    Create SplashActivity.kt

---
+++ SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.perpustakaanrajesh
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
